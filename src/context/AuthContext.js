ef92c805f5c15cc171306767a95d5240a3100e27
Nitesh Singh <nitesh.datagenit@gmail.com>
2024-09-26 16:35:41 +0530

    committed by nitesh

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,19 +1,21 @@
 import { createContext, useEffect, useState } from "react";
-import { getCookie } from "../utils/Utils";
-
+import { getCookie ,setCookie } from "../utils/Utils";
 export const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState({});
 
   useEffect(() => {
-    const userCookie = getCookie('user');
+    const userCookie = getCookie("user");
     const userInfo = userCookie ? JSON.parse(userCookie) : null;
     setCurrentUser(userInfo?.data);
   }, []);
-
+  const updateUser = (data) => {
+    setCurrentUser(data);
+    setCookie("user", JSON.stringify(data), 7);
+  };
   return (
-    <AuthContext.Provider value={{ currentUser }}>
+    <AuthContext.Provider value={{ currentUser, updateUser }}>
       {children}
     </AuthContext.Provider>
   );
